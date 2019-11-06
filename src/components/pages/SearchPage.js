60067128732c891d789264ca6eb9b9eae5c6406d
nazmul-nm <mdnazmul.nm@gmail.com>
2019-11-06 17:48:43 +0600

    search

--- src/components/pages/SearchPage.js
+++ src/components/pages/SearchPage.js
@@ -1,7 +1,33 @@
 import React, { Component } from 'react'
-
+import Icofont from 'react-icofont';
+import $ from "jquery";
 export class SearchPage extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+             isPhone:false,
+        }
+    
+    }
+    
+    showPhone = (e) => {
+        var parent, child;
+        //  d = e.currentTarget.dataset.id 
+        // alert(e.target.getAttribute('data-id'));
+        $('.phoneNumberArea').removeClass('d_flex');
+        $('.phoneNumber').addClass('hidden');
+        parent = $(e.target).closest('.single_search').find('.phoneNumberArea');
+        parent.toggleClass('d_flex');
+        child = $(e.target).closest('.single_search').find('.phoneNumber');
+        child.toggleClass('hidden');
+        
+    }
+
+
     render() {
+        const {isPhone } = this.state;
         return (
             <div>
                 <div className="container mt-50" >
@@ -10,14 +36,18 @@ export class SearchPage extends Component {
                             <div className="single_search">
                                 <div className="serch_left_area">
                                     <img src="https://via.placeholder.com/80x80" alt='' />
+                                    <Icofont icon="icofont-male" />
                                 </div>
                                 <div className="right_search_area">
                                     <div className="top_search">
                                         <h4>Username</h4>
                                         <h4>O+</h4>
                                     </div>
-                                    <div className="phoneNumber">
-                                        <h5>01919821479</h5>
+                                    <div className="phoneNumberArea"> 
+                                        <button type="button"  onClick={this.showPhone}>Phone Number</button>
+                                        <div className="phoneNumber hidden">
+                                            <h5>01919821479</h5>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
@@ -32,8 +62,12 @@ export class SearchPage extends Component {
                                         <h4>Username</h4>
                                         <h4>O+</h4>
                                     </div>
-                                    <div className="phoneNumber">
-                                        <h5>01919821479</h5>
+                                    <div className="phoneNumberArea"> 
+                                        <button type="button"  onClick={this.showPhone}>Phone Number</button>
+                                        <div className="phoneNumber hidden">
+                                            <h5>01919821479</h5>
+                                        </div>
+                                        
                                     </div>
                                 </div>
                             </div>
@@ -48,8 +82,11 @@ export class SearchPage extends Component {
                                         <h4>Username</h4>
                                         <h4>O+</h4>
                                     </div>
-                                    <div className="phoneNumber">
-                                        <h5>01919821479</h5>
+                                    <div className="phoneNumberArea"> 
+                                        <button type="button"  onClick={this.showPhone}>Phone Number</button>
+                                        <div className="phoneNumber hidden">
+                                            <h5>01919821479</h5>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
